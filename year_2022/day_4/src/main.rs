40c84fc4b452881b35842cc250acd640b8f349a5
Daison <daisonthms@gmail.com>
2023-06-07 07:06:09 +0530

    day 4

---
+++ year_2022/day_4/src/main.rs
@@ -0,0 +1,27 @@
+// --- Day 4: Camp Cleanup ---
+// https://adventofcode.com/2022/day/4
+
+use std::fs;
+use std::io::{Read, Result};
+
+fn main() -> Result<()> {
+    let mut input_file = fs::File::open("./src/input")?;
+    let mut data = String::new();
+    input_file.read_to_string(&mut data)?;
+
+    let mut count = 0;
+
+    for line in data.lines() {
+        let p: Vec<&str> = line.split(',').map(|x| x).collect();
+        let a: Vec<i32> = p[0].split('-').map(|x| x.parse::<i32>().unwrap()).collect();
+        let b: Vec<i32> = p[1].split('-').map(|x| x.parse::<i32>().unwrap()).collect();
+
+        if a[0] <= b[0] && a[1] >= b[1] || b[0] <= a[0] && b[1] >= a[1] {
+            count += 1;
+        }
+    }
+
+    println!("{} pairs fully contain the other", count);
+
+    Ok(())
+}
