33aaeab3988a6d97365284ddf73bbb889320df1b
Daison <daisonthms@gmail.com>
2023-06-07 07:29:37 +0530

    day 4

--- year_2022/day_4/src/main.rs
+++ year_2022/day_4/src/main.rs
@@ -9,7 +9,8 @@ fn main() -> Result<()> {
     let mut data = String::new();
     input_file.read_to_string(&mut data)?;
 
-    let mut count = 0;
+    let mut count_p1 = 0;
+    let mut count_p2 = 0;
 
     for line in data.lines() {
         let p: Vec<&str> = line.split(',').map(|x| x).collect();
@@ -17,11 +18,16 @@ fn main() -> Result<()> {
         let b: Vec<i32> = p[1].split('-').map(|x| x.parse::<i32>().unwrap()).collect();
 
         if a[0] <= b[0] && a[1] >= b[1] || b[0] <= a[0] && b[1] >= a[1] {
-            count += 1;
+            count_p1 += 1;
+        }
+
+        if a[0] >= b[0] && a[0] <= b[1] || b[0] >= a[0] && b[0] <= a[1] {
+            count_p2 += 1;
         }
     }
 
-    println!("{} pairs fully contain the other", count);
+    println!("{} pairs fully contain the other", count_p1);
+    println!("{} pairs overlap the other", count_p2);
 
     Ok(())
 }
