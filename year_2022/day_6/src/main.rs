7c3b72db01394680fd48d4499bde5a961e7c7e17
Daison <daisonthms@gmail.com>
2023-06-09 08:12:13 +0530

    day 6 part 1 refactor

--- year_2022/day_6/src/main.rs
+++ year_2022/day_6/src/main.rs
@@ -7,25 +7,22 @@ use std::{
 };
 
 fn main() -> Result<()> {
-    let mut input_file = fs::File::open("./src/input")?;
+    let mut input_file: fs::File = fs::File::open("./src/input")?;
     let mut data = String::new();
     input_file.read_to_string(&mut data)?;
 
-    let iter = data.chars().collect::<Vec<char>>();
-    let sliding_window = iter.windows(4);
-
     let mut num: i32 = 0;
 
-    for window in sliding_window {
+    for window in data.chars().collect::<Vec<char>>().windows(4) {
         num += 1;
-        let mut repeat = false;
+        let mut repeat: bool = false;
         let mut x = 1;
         for ch in window {
-            (x..=3).for_each(|y| {
+            for y in x..=3 {
                 if ch.eq(&window[y]) {
                     repeat = true;
                 }
-            });
+            }
             x += 1;
         }
         if !repeat {
