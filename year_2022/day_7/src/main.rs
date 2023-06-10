cbb0801376eecbfa198c4529bfd66ac08cd68640
Daison <daisonthms@gmail.com>
2023-06-10 20:09:15 +0530

    day 7 part2 refactor

--- year_2022/day_7/src/main.rs
+++ year_2022/day_7/src/main.rs
@@ -12,22 +12,14 @@ fn main() -> Result<()> {
     input_file.read_to_string(&mut data)?;
 
     let mut stack: Vec<(&str, i32)> = Vec::new();
-    let mut top = 0;
+    let mut top: usize = 0;
 
-    let mut used_size = 0;
+    let used_space = calculate_used_space(data.clone());
 
-    for line in data.lines() {
-        let words = line.split_whitespace().collect::<Vec<&str>>();
-        match words[0] {
-            "$" | "dir" => {}
-            x => used_size += x.parse::<i32>().unwrap(),
-        }
-    }
+    let available_space = 70000000 - used_space;
+    let delete_space = 30000000 - available_space;
 
-    let available_size = 70000000 - used_size;
-    let delete_size = 30000000 - available_size;
-
-    let mut ddir_size: i32 = used_size;
+    let mut ddir_space: i32 = used_space;
 
     let mut sum: i32 = 0;
 
@@ -38,16 +30,18 @@ fn main() -> Result<()> {
             "$" => match words[1] {
                 "cd" => match words[2] {
                     ".." => {
-                        stack[top - 1].1 += stack[top].1;
-                        top -= 1;
-                        let poped = stack.pop().unwrap();
-
-                        if poped.1 >= delete_size && poped.1 <= ddir_size {
-                            ddir_size = poped.1;
+                        if top > 0 {
+                            stack[top - 1].1 += stack[top].1;
+                            top -= 1;
                         }
 
+                        let poped = stack.pop().unwrap();
                         if poped.1 <= 100000 {
                             sum += poped.1;
+                        }
+
+                        if poped.1 >= delete_space && poped.1 <= ddir_space {
+                            ddir_space = poped.1;
                         }
                     }
                     word => {
@@ -68,12 +62,9 @@ fn main() -> Result<()> {
     }
 
     let mut len = stack.len();
-    loop {
-        if len == 0 {
-            break;
-        }
 
-        if len > 1 {
+    while len != 0 {
+        if top > 0 {
             stack[top - 1].1 += stack[top].1;
             top -= 1;
         }
@@ -83,23 +74,26 @@ fn main() -> Result<()> {
             sum += poped.1;
         }
 
-        if poped.1 >= delete_size && poped.1 <= ddir_size {
-            ddir_size = poped.1;
+        if poped.1 >= delete_space && poped.1 <= ddir_space {
+            ddir_space = poped.1;
         }
-
         len -= 1;
     }
 
-    println!("SUM = {sum}");
-    println!("need to be deleted = {delete_size}");
-    println!("delete file size = {ddir_size}");
+    println!("Part 1: SUM = {sum}");
+    println!("Part 2: Size of directory to be deleted  = {ddir_space}");
 
     Ok(())
 }
 
-// fn print_stack(stack: Vec<(&str, i32)>) {
-//     println!("");
-//     for i in stack.iter() {
-//         println!("{:?}", i);
-//     }
-// }
+fn calculate_used_space(data: String) -> i32 {
+    let mut used_space = 0;
+    for line in data.lines() {
+        let words = line.split_whitespace().collect::<Vec<&str>>();
+        match words[0] {
+            "$" | "dir" => {}
+            x => used_space += x.parse::<i32>().unwrap(),
+        }
+    }
+    used_space
+}
