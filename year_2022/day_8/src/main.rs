0168a1eca10220527de3b8603c86523fa26c7df4
Daison <daisonthms@gmail.com>
2023-06-11 18:09:09 +0530

    day 8 part1

--- year_2022/day_8/src/main.rs
+++ year_2022/day_8/src/main.rs
@@ -11,5 +11,72 @@ fn main() -> Result<()> {
     let mut data = String::new();
     input_file.read_to_string(&mut data)?;
 
+    let mut matrix: Vec<Vec<i32>> = Vec::new();
+
+    for line in data.lines() {
+        let ar = line
+            .chars()
+            .map(|a| a.to_digit(10).unwrap() as i32)
+            .collect::<Vec<i32>>();
+        matrix.push(ar)
+    }
+
+    let len = matrix.len();
+    let wid = matrix[1].len();
+    println!("length : {len}");
+    println!("width : {wid}\n");
+
+    let mut count = (len * 2) + ((wid - 2) * 2);
+    let mut g = 0;
+    for j in 1..len - 1 {
+        for k in 1..wid - 1 {
+            g += 1;
+            let num = matrix[j][k];
+            let mut t: bool = true;
+            let mut b: bool = true;
+            let mut r: bool = true;
+            let mut l: bool = true;
+
+            //check to top
+            for x in 0..j {
+                if matrix[x][k] >= num {
+                    t = false;
+                }
+            }
+
+            //check to bottom
+            for x in j + 1..len {
+                if matrix[x][k] >= num {
+                    b = false;
+                }
+            }
+
+            //check to left
+            for x in 0..k {
+                if matrix[j][x] >= num {
+                    l = false;
+                }
+            }
+
+            //check to right
+            for x in k + 1..wid {
+                if matrix[j][x] >= num {
+                    r = false;
+                }
+                // println!("{} at {}", num, matrix[j][x]);
+            }
+
+            if r || l || t || b {
+                count += 1;
+                // println!("{num} at [{j}][{k}]");
+            }
+        }
+    }
+    println!("count : {count}");
+    println!("loops : {g}");
+
+    // for ar in matrix.iter() {
+    //     println!("{:?}", ar);
+    // }
     Ok(())
 }
