69b3f17d65ea021e52555f5e74b9fca6730a53aa
Daison <daisonthms@gmail.com>
2023-06-13 21:27:28 +0530

    day 9 remake

--- year_2022/day_9/src/main.rs
+++ year_2022/day_9/src/main.rs
@@ -6,7 +6,6 @@
 pub mod part2;
 
 use std::{
-    cmp,
     fs::File,
     io::{Read, Result},
 };
@@ -31,7 +30,6 @@ fn main() -> Result<()> {
         let num = parts[1].parse::<i32>().unwrap();
 
         for _ in 0..num {
-            let head = h;
             match direction {
                 "R" => h.1 += 1,
                 "L" => h.1 -= 1,
@@ -39,75 +37,60 @@ fn main() -> Result<()> {
                 "U" => h.0 -= 1,
                 _ => {}
             }
-            if h_is_not_near(h, t) {
-                t = head;
-                position.push(t);
-            }
+            t = get_next_tail_position(t, h);
+            position.push(t);
         }
     }
 
     position.sort();
     position.dedup();
+
     println!("Part 1 : Number of positions : {}", position.len());
 
-    // part2::part_2(data);
-
-    let f: i32 = 5;
-    let g: i32 = -3;
-    println!("result : {}", g - f);
-    println!("result : {}", f.abs() - g.abs());
-    println!("result : {}", f.abs_diff(g));
-    println!("result : {}", g.abs_diff(f));
+    part2::part_2(data);
 
     Ok(())
 }
 
-// fn get_next_tail_position(t: Point<i32>, h: Point<i32>) -> Point<i32> {
-//     let new: Point<i32> = (0, 0);
-//     // let mut dx = h.0 - t.0;
-//     // let mut dy = h.0 - t.0;
-//
-//     new
-// }
-
-fn get_next_tail_position(t: Point<i32>, h: Point<i32>) -> Point<i32> {
+fn get_next_tail_position(mut t: Point<i32>, h: Point<i32>) -> (i32, i32) {
     let dx = h.0 - t.0;
     let dy = h.1 - t.1;
 
-    if !dx.abs() <= 1 && !dy.abs() <= 1 {
+    if dx.abs() <= 1 && dy.abs() <= 1 {
         return t;
-    }
-
-    if dx > 1 {
+    } else if dx > 1 {
         t.0 += 1;
-        if dy >= 1 {
+        if dy > 0 {
             t.1 += 1;
         }
-        if dy <= -1 {}
+        if dy < 0 {
+            t.1 -= 1;
+        }
+    } else if dx < -1 {
+        t.0 -= 1;
+        if dy > 0 {
+            t.1 += 1;
+        }
+        if dy < 0 {
+            t.1 -= 1;
+        }
+    } else if dy > 1 {
+        t.1 += 1;
+        if dx > 0 {
+            t.0 += 1;
+        }
+        if dx < 0 {
+            t.0 -= 1;
+        }
+    } else if dy < -1 {
+        t.1 -= 1;
+        if dx > 0 {
+            t.0 += 1;
+        }
+        if dx < 0 {
+            t.0 -= 1;
+        }
     }
 
     return t;
 }
-
-// fn h_is_not_near(t: Point<i32>, h: Point<i32>) -> bool {
-//     if t == h {
-//         return false;
-//     }
-//
-//     let difference;
-//
-//     if t.0 == h.0 {
-//         difference = h.1.abs_diff(t.1);
-//     } else if t.1 == h.1 {
-//         difference = h.0.abs_diff(t.0);
-//     } else {
-//         let d1 = h.0.abs_diff(t.0);
-//         let d2 = h.1.abs_diff(t.1);
-//         difference = d1 * d2;
-//     }
-//
-//     match difference {
-//         1 => return false,
-//         _ => return true,
-//     }
-// }
--- year_2022/day_9/src/part2.rs
+++ year_2022/day_9/src/part2.rs
@@ -3,7 +3,7 @@
 
 // PART 2
 
-use std::cmp;
+use crate::get_next_tail_position;
 
 type Point<T> = (T, T);
 
@@ -11,23 +11,16 @@ pub fn part_2(data: String) {
     let s: Point<i32> = (0, 0);
 
     let mut knot: [Point<i32>; 10] = [s; 10];
+
     let mut position: Vec<Point<i32>> = Vec::new();
     position.push(s);
-
-    let mut n = 1;
 
     for line in data.lines() {
         let parts = line.split_whitespace().collect::<Vec<&str>>();
         let direction = parts[0];
         let num = parts[1].parse::<i32>().unwrap();
 
-        println!("count : {n} {direction}");
-        n += 1;
-
-        let mut w = 1;
         for _ in 0..num {
-            let knot_temp: [Point<i32>; 10] = knot;
-
             match direction {
                 "R" => knot[0].1 += 1,
                 "L" => knot[0].1 -= 1,
@@ -37,22 +30,12 @@ pub fn part_2(data: String) {
             }
 
             for i in 1..=9 {
-                if h_is_not_near(knot[i - 1], knot_temp[i]) {
-                    knot[i] = knot_temp[i - 1];
-                } else {
-                    continue;
-                }
+                knot[i] = get_next_tail_position(knot[i], knot[i - 1]);
+
                 if i == 9 {
                     position.push(knot[9]);
                 }
             }
-
-            print!("{w} | ");
-            for i in knot.iter() {
-                print!("{:?} ", i);
-            }
-            println!("");
-            w += 1;
         }
     }
 
@@ -60,41 +43,4 @@ pub fn part_2(data: String) {
     position.dedup();
 
     println!("Part 2 : Number of positions : {}", position.len());
-}
-
-fn next_tail_position() {}
-
-fn h_is_not_near(h: Point<i32>, t: Point<i32>) -> bool {
-    if t == h {
-        return false;
-    }
-
-    let mut max;
-    let mut min;
-    let difference;
-
-    if t.0 == h.0 {
-        max = cmp::max(h.1, t.1);
-        min = cmp::min(h.1, t.1);
-        difference = max - min;
-    } else if t.1 == h.1 {
-        max = cmp::max(h.0, t.0);
-        min = cmp::min(h.0, t.0);
-        difference = max - min;
-    } else {
-        max = cmp::max(h.0, t.0);
-        min = cmp::min(h.0, t.0);
-        let d1 = max - min;
-        max = cmp::max(h.1, t.1);
-        min = cmp::min(h.1, t.1);
-        let d2 = max - min;
-        difference = d1 * d2;
-    }
-
-    match difference {
-        1 => {
-            return false;
-        }
-        _ => return true,
-    }
 }
