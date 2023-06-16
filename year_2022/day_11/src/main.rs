b7f216cb5a741179bfd6041340ae45b7ac7040f0
Daison <daisonthms@gmail.com>
2023-06-16 10:28:42 +0530

    day 11 p1

--- year_2022/day_11/src/main.rs
+++ year_2022/day_11/src/main.rs
@@ -6,62 +6,123 @@ use std::{
     io::{Read, Result},
 };
 
-struct Monkey<'a> {
-    items: &'a Vec<i32>,
+struct Monkey {
+    items: Vec<i32>,
     inspection_count: i32,
-    operation: String,
+    operation: char,
+    operand: String,
     divisible: i32,
     if_true: i32,
     if_false: i32,
 }
-
-// impl Monkey {
-//     fn get_item(&mut self) -> i32 {
-//         self.items.pop().unwrap()
-//     }
-// }
 
 fn main() -> Result<()> {
     let mut input_file = File::open("./src/input")?;
     let mut data = String::new();
     input_file.read_to_string(&mut data)?;
 
-    // let mut monkeys: Vec<Monkey> = get_each_monkey_data(data);
     let mut monkeys: Vec<Monkey> = Vec::new();
 
     for para in data.split("\n\n").collect::<Vec<&str>>().iter() {
-        monkeys.insert(0, get_each_monkey_data(para));
+        monkeys.push(get_each_monkey_data(para));
     }
 
-    for monkey in monkeys.iter() {
-        monkey.items.pop().unwrap();
+    for r in 0..20 {
+        for n in 0..monkeys.len() {
+            for _ in 0..monkeys[n].items.len() {
+                let item = monkeys[n].items.pop().unwrap();
+
+                let operand;
+                match monkeys[n].operand.as_str() {
+                    "old" => operand = item,
+                    _ => operand = monkeys[n].operand.parse::<i32>().unwrap(),
+                }
+
+                let mut worry_level: i32 = 0;
+                match monkeys[n].operation {
+                    '*' => worry_level = item * operand,
+                    '+' => worry_level = item + operand,
+                    _ => (),
+                }
+                // println!("operand : {operand}");
+
+                worry_level /= 3;
+                println!("worry_level : {worry_level}");
+
+                let val = worry_level % monkeys[n].divisible;
+
+                let if_true = monkeys[n].if_true as usize;
+                let if_false = monkeys[n].if_false as usize;
+                match val != 0 {
+                    true => monkeys[if_true].items.insert(0, worry_level),
+                    false => monkeys[if_false].items.insert(0, worry_level),
+                }
+                monkeys[n].inspection_count += 1;
+            }
+        }
+
+        println!("for round: {}", r + 1);
+        for n in 0..monkeys.len() {
+            print!("monkey {n}:");
+            for m in monkeys[n].items.iter() {
+                print!(" {m}");
+            }
+            println!("");
+        }
+        println!("");
+    }
+
+    for m in monkeys.iter() {
+        println!("{}", m.inspection_count);
     }
 
     Ok(())
 }
 
-fn get_each_monkey_data<'a>(para: &str) -> Monkey {
+fn get_each_monkey_data(para: &str) -> Monkey {
+    // println!("{para}");
     let mut items: Vec<i32> = Vec::new();
     let inspection_count = 0;
 
     for l in para.lines().nth(1).unwrap().split_whitespace() {
         if l.chars().nth(0).unwrap().is_numeric() {
-            items.insert(0, l.split(',').nth(0).unwrap().parse::<i32>().unwrap());
+            items.push(l.split(',').nth(0).unwrap().parse::<i32>().unwrap());
         }
     }
 
     let operation = para
         .lines()
-        .nth(3)
+        .nth(2)
         .unwrap()
-        .split("old")
+        .split("=")
         .nth(1)
+        .unwrap()
+        .split_whitespace()
+        .nth(1)
+        .unwrap()
+        .chars()
+        .nth(0)
+        .unwrap();
+
+    // println!("{operation}");
+
+    let operand = para
+        .lines()
+        .nth(2)
+        .unwrap()
+        .split("=")
+        .nth(1)
+        .unwrap()
+        .split_whitespace()
+        .nth(2)
         .unwrap()
         .to_string();
 
+    // println!("{operand}");
+
     let divisible = para
         .lines()
-        .nth(4)
+        .nth(3)
         .unwrap()
         .split_whitespace()
         .last()
@@ -71,7 +132,7 @@ fn get_each_monkey_data<'a>(para: &str) -> Monkey {
 
     let if_true = para
         .lines()
-        .nth(5)
+        .nth(4)
         .unwrap()
         .split_whitespace()
         .last()
@@ -81,7 +142,7 @@ fn get_each_monkey_data<'a>(para: &str) -> Monkey {
 
     let if_false = para
         .lines()
-        .nth(6)
+        .nth(5)
         .unwrap()
         .split_whitespace()
         .last()
@@ -93,6 +154,7 @@ fn get_each_monkey_data<'a>(para: &str) -> Monkey {
         items,
         inspection_count,
         operation,
+        operand,
         divisible,
         if_true,
         if_false,
